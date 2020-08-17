46cb732a5d3ce2b8ce1878c796a9e88fac27423e
compmolsci <rmaitra@chem.iitb.ac.in>
2020-08-17 12:13:40 +0530

    reduced the itertools function, and reduced code length for 1D hubbard_model

--- hubbard_hamiltonian.py
+++ hubbard_hamiltonian.py
@@ -1,18 +1,29 @@
 import numpy as np
 import cmath
 from itertools import product
-from inp import spin_sites
+from inp import spin_sites,spatial_sites
 from products import multiply_two_operators, multiply_four_operators
 from products import clean_up
 
 #Hopping terms
 print("Hopping terms:")
-a=np.array(list(product(range(spin_sites), repeat=2)))
+def hopping_hh():
+    hopping=[]
+    for i in range(spin_sites):
+        if i<=spin_sites-2:
+            hopping.append(([i,1],[(i+2)%spin_sites,0]))
+        if i>=2:
+            hopping.append(([i,1],[(i-2)%spin_sites,0]))
+    #print(np.array(hopping).shape)
+    return np.array(hopping)
+print(hopping_hh())
 hopping=[]
+
+a=np.array(list(product(range(spin_sites), repeat=2)))
 for i in range (a.shape[0]): #Generates all the combinations of hopping term (doesn't consider periodic boundary condition)
     if ((a[i][0]-a[i][1]==2) or (a[i][0]-a[i][1]==-2)):
         hopping.append(a[i])
-
+print(hopping)
         
 if (spin_sites!=4):  # For including the periodic boundary conditions
     hopping.append([spin_sites-1,1])
@@ -28,12 +39,19 @@ for i in range (hopping.shape[0]):
     p=hopping[i][0]
     q=hopping[i][1]
     m.append(multiply_two_operators([p,True],[q,False]))
-print(m)
+#print(m)
 
 
 
 #On-site terms
 print("Onsite terms:")
+def onsite_hh():
+    onsite=[]
+    for i in range(spatial_sites):
+        onsite.append(([2*i,1],[2*i,0],[2*i+1,1],[2*i+1,0]))
+    return np.array(onsite)
+print(onsite_hh())
+
 b=np.array(list(product(range(spin_sites),repeat=4)))
 onsite=[]
 for i in range (b.shape[0]):
@@ -49,7 +67,7 @@ for i in range (onsite.shape[0]):
     r=onsite[i][2]
     s=onsite[i][3]
     m1.append(multiply_four_operators([p,True],[q,False],[r,True],[s,False]))
-print(m1)
+#print(m1)
 
 
 
